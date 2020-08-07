64357b74df4ce3e0527ed7b3a76a75f0faae186f
Pradnya1208 <pradnyapatil671@gmail.com>
2020-08-07 22:23:20 +0530

    Saving covid19 data of Maharashtra's cities in Redis Hash

---
+++ basics.py
@@ -0,0 +1,55 @@
+import redis
+client = redis.Redis(host = '127.0.0.1', port = '6379')
+import requests
+import json
+import pandas as pd
+from pandas import DataFrame as df
+
+JSON_URL = 'https://api.covid19india.org/v5/min/timeseries-MH.min.json'
+dist_name  = []
+
+req = requests.get(JSON_URL)
+
+
+distNames = df(req.json()['MH']['districts'])
+distNames = distNames.T
+
+
+for dis in distNames.index:
+    dist_name.append(dis)
+
+
+for dist  in dist_name:   
+    
+    if not 'Unknown' in dist:
+
+        covid = df(req.json()['MH']['districts'][dist])
+                     
+        for conf, date in zip(covid.dates, covid.index):
+            for t in conf.keys():
+                if 'total' in t:
+                    dist_c = dist + "_confirmed"
+                    dist_d = dist + "_deceased"
+                    dist_r = dist + "_recovered"
+                    if 'confirmed' in (conf['total'].keys()):
+                        client.hsetnx(dist_c, date, conf['total']['confirmed'])
+                    if not 'confirmed' in (conf['total'].keys()):
+                        client.hsetnx(dist_c, date, "0")
+                        
+                    if 'recovered' in (conf['total'].keys()):
+                        client.hsetnx(dist_r, date, conf['total']['recovered'])
+                    if not 'recovered' in (conf['total'].keys()):
+                        client.hsetnx(dist_r, date, "0")
+                        
+                    if 'deceased' in (conf['total'].keys()):
+                        client.hsetnx(dist_d, date, conf['total']['deceased'])
+                    if not 'deceased' in (conf['total'].keys()):
+                        client.hsetnx(dist_r, date, "0")
+                    
+                   
+
+
+
+    
+
+
